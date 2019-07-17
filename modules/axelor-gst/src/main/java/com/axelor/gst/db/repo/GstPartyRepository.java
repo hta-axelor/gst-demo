6d8e4f10b7077ae2197b6bdab8ebf46016a61b92
hemantkumartank <hta.axelor@gmail.com>
2019-07-17 16:54:13 +0530

    Create card view for product

--- modules/axelor-gst/src/main/java/com/axelor/gst/db/repo/GstPartyRepository.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/repo/GstPartyRepository.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import com.axelor.gst.db.Party;
 
-public class PartyCardRepository extends PartyRepository{
+public class GstPartyRepository extends PartyRepository{
 	@Override
 	public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
 		if (!context.containsKey("json-enhance")) {
@@ -13,10 +13,7 @@ public class PartyCardRepository extends PartyRepository{
 		try {
 			Long id = (Long) json.get("id");
 			Party party = find(id);
-			json.put("name", party.getName());
-			json.put("type", party.getType());
-			json.put("gstin", party.getGstin());
-			json.put("contactList", party.getContactList().get(0));
+			json.put("contact", party.getContactList().get(0));
 		} catch (Exception e) {}
 		return json;
 	}
